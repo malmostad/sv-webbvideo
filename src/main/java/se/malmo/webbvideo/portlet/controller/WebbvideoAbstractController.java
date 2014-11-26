443ec90d021e87ed11e7116763bf8eba0caee073
Henrik <henrik.rydstedt@gmail.com>
2014-11-27 00:23:56 +0100

    Updated comments and return of null in getCategories().

--- src/main/java/se/malmo/webbvideo/portlet/controller/WebbvideoAbstractController.java
+++ src/main/java/se/malmo/webbvideo/portlet/controller/WebbvideoAbstractController.java
@@ -21,6 +21,12 @@ public abstract class WebbvideoAbstractController {
     @Autowired VideoCloudCategoriesRequestService categoriesService;
     @Autowired VideoCloudCategoryVideoRequestService videoService;
     
+    /**
+     * Retrieves id and name on all playlists from VideoCloud's API with the specified token.
+     * @param token
+     * @return  Map with id as key and name as value, 
+     *          if no categories could be retrieved null is returned.
+     */
     protected Map<String, String> getCategories(String token) {
         FindAllPlaylistsBuilder queryBuilder = new FindAllPlaylistsBuilder(token);
         PlaylistFieldsBuilder pfb = new PlaylistFieldsBuilder();
@@ -35,10 +41,19 @@ public abstract class WebbvideoAbstractController {
             for(Items item : items) {
                 categoryMap.put(item.getId(), item.getName());
             }
+            return categoryMap;
         }
-        return categoryMap;
+        return null;
     }
-
+    
+    /**
+     * Retrieves a playlist's videos from VideoCloud's API with corresponding id and token. 
+     * The amount of videos returned is set by numberOfVideos.
+     * @param id
+     * @param token
+     * @param numberOfVideos
+     * @return List of Videos
+     */
     protected List<Videos> getVideosFromPlaylistId(String id, String token, int numberOfVideos) {
         FindPlaylistByIdBuilder queryBuilder = new FindPlaylistByIdBuilder(id, token);
         PlaylistFieldsBuilder pfb = new PlaylistFieldsBuilder();
