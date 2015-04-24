4fb9992b3a66fa52d0e436183bfc1787e629b536
Henrik <henrik.rydstedt@gmail.com>
2015-04-24 16:16:30 +0200

    Handling values from property file

--- src/main/java/se/malmo/webbvideo/portlet/controller/WebbvideoRequestController.java
+++ src/main/java/se/malmo/webbvideo/portlet/controller/WebbvideoRequestController.java
@@ -13,6 +13,7 @@ import javax.portlet.RenderResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import javax.portlet.PortletPreferences;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import se.malmo.webbvideo.portlet.exception.VideoCloudRequestException;
@@ -30,6 +31,9 @@ import senselogic.sitevision.api.resource.ResourceLocatorUtil;
 @RequestMapping("VIEW")
 @SessionAttributes("category")
 public class WebbvideoRequestController extends WebbvideoAbstractController {
+    @Value("${webbvideo.number_of_videos}") private Integer number_of_videos;
+    @Value("${webbvideo.video_class}") private String video_class;
+    @Value("${webbvideo.filter}") private String[] filter;
     
     @RequestMapping()
     public String showItems(Model model, RenderRequest request, RenderResponse response, PortletPreferences prefs) {
@@ -38,7 +42,7 @@ public class WebbvideoRequestController extends WebbvideoAbstractController {
         ResourceLocatorUtil rlu = utils.getResourceLocatorUtil();
         Node currentPage = pcu.getCurrentPage();
         Node sitePage = rlu.getSitePage();
-
+        
         String category =  prefs.getValue(CATEGORY, null);
         List<Videos> videoList = null;
         if(category != null) {
@@ -46,8 +50,8 @@ public class WebbvideoRequestController extends WebbvideoAbstractController {
             if(currentPage == sitePage) {
                 List<Videos> generalVideoList;
                 try {
-                    videoList = getVideosFromPlaylistId(category, ACCESS_TOKEN_GENERAL, NUMBER_OF_VIDEOS-1);
-                    generalVideoList = getVideosFromPlaylistId(KF_PLAYLIST_ID, ACCESS_TOKEN_KF, 1);
+                    videoList = getVideosFromPlaylistId(category, ACCESS_TOKEN_GENERAL, number_of_videos-1, filter);
+                    generalVideoList = getVideosFromPlaylistId(KF_PLAYLIST_ID, ACCESS_TOKEN_KF, 1, filter);
                 } catch (VideoCloudRequestException ex) {
                     Logger.getLogger(WebbvideoRequestController.class.getName()).log(Level.SEVERE, null, ex);
                     model.addAttribute("errorText", ex.getMessage());
@@ -59,12 +63,15 @@ public class WebbvideoRequestController extends WebbvideoAbstractController {
             /* OTHER PAGES */
             else {
                 try {
-                    videoList = getVideosFromPlaylistId(category, ACCESS_TOKEN_GENERAL, NUMBER_OF_VIDEOS);
+                    videoList = getVideosFromPlaylistId(category, ACCESS_TOKEN_GENERAL, number_of_videos, filter);
                 } catch (VideoCloudRequestException ex) {
                     Logger.getLogger(WebbvideoRequestController.class.getName()).log(Level.SEVERE, null, ex);
+                    model.addAttribute("errorText", ex.getMessage());
+                    return "error";
                 }
             }
-
+            
+            model.addAttribute("video_class", video_class);
             model.addAttribute("videoList", videoList);
             return "videolist";
         }
